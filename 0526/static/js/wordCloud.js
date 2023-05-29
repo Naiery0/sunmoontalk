f8d6919262d789ad7680d45a12bb0af677f5e9b9
Naiery0 <molly724@naver.com>
2023-05-29 14:24:13 +0900

    0529 pm2

--- 0526/static/js/wordCloud.js
+++ 0526/static/js/wordCloud.js
@@ -1,26 +1,37 @@
-// 지금은 임시로 html에 병합돼있음
-// wordCloud.js
+    // 서버에 워드클라우드 데이터 요청
+    fetch('/wordcloud')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('워드클라우드 데이터 요청 실패');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // 워드클라우드 생성
+        createWordCloud(data.wordCounts);
+      })
+      .catch((error) => {
+        // 에러 발생 시 처리할 내용을 작성하세요.
+        console.error('워드클라우드 데이터 요청 에러', error);
+      });
 
-// 서버에 워드클라우드 데이터 요청
-fetch('/wordcloud')
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error('워드클라우드 데이터 요청 실패');
+    // 워드클라우드 생성 함수
+    function createWordCloud(wordCounts) {
+      // 워드클라우드 생성 옵션 설정
+      const options = {
+        list: Object.entries(wordCounts),
+        gridSize: 10,
+        weightFactor: 5,
+        fontFamily: 'Arial',
+        color: 'random-dark',
+        rotateRatio: 0.5,
+        backgroundColor: '#fff',
+        shuffle: true,
+        shape: 'square',
+        drawOutOfBound: false,
+        origin: [400, 250]
+      };
+
+      // 워드클라우드 생성
+      WordCloud(document.getElementById('wordcloud'), options);
     }
-    return response.json();
-  })
-  .then((data) => {
-    // 워드클라우드 생성 및 표시
-    createWordCloud(data.wordCounts);
-  })
-  .catch((error) => {
-    // 에러 발생 시 처리할 내용을 작성하세요.
-    console.error('워드클라우드 데이터 요청 에러', error);
-  });
-
-// 워드클라우드 생성 함수
-function createWordCloud(wordCounts) {
-  // 워드클라우드 생성 로직을 구현하세요.
-  // wordCounts 객체를 활용하여 단어와 빈도수에 따라 워드클라우드를 생성하세요.
-  // 워드클라우드를 표시할 HTML 요소에 결과를 렌더링하세요.
-}
