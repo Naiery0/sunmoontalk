9416ad61c0473df6e251f5c22e720c19f344e070
{Naiery0} <{molly724@naver.com}>
2023-05-31 14:44:39 +0900

    비변닉변 오류 수정, DB작업 해야됨

--- 0526/static/js/myPage.js
+++ 0526/static/js/myPage.js
@@ -79,87 +79,87 @@ displayInfo();
 
 function changePW() {
     var changeBtn = document.createElement("input");
-    changeBtn.setAttribute("class", "btn");
+    changeBtn.setAttribute("class", "btn save pw");
     changeBtn.setAttribute("type", "button");
     changeBtn.setAttribute("value", "저장하기");
     changeBtn.setAttribute("onclick", "savePW()");
-    
+
     var pwLabel = document.getElementById("pw");
     var pwInput = document.createElement("input");
     pwInput.setAttribute("class", "inputField");
     pwInput.setAttribute("type", "password");
     pwInput.setAttribute("id", "newPw");
-    
+
     var wrap = document.getElementsByClassName("wrap")[0];
-    
-    wrap.replaceChild(changeBtn, document.getElementsByClassName("pw")[0]);
+
+    wrap.replaceChild(changeBtn, document.getElementsByClassName("btn pw")[0]);
     wrap.replaceChild(pwInput, pwLabel);
   }
-  
+
   function savePW() {
     var newPassword = document.getElementById("newPw").value;
-    
-    // 여기에 비밀번호 저장 또는 처리하는 코드를 추가하세요.
+
+    // 여기에 비밀번호 저장 
+
     
     var changeBtn = document.createElement("input");
     changeBtn.setAttribute("class", "btn pw");
     changeBtn.setAttribute("type", "button");
     changeBtn.setAttribute("value", "변경하기");
     changeBtn.setAttribute("onclick", "changePW()");
-    
+
     var pwInput = document.getElementById("newPw");
     var pwLabel = document.createElement("span");
     pwLabel.setAttribute("class", "label2");
     pwLabel.setAttribute("id", "pw");
     pwLabel.innerText = "비밀번호정보";
-    
+
     var wrap = document.getElementsByClassName("wrap")[0];
-    
-    wrap.replaceChild(changeBtn, document.getElementsByClassName("btn pw")[0]);
+
+    wrap.replaceChild(changeBtn, document.getElementsByClassName("btn save pw")[0]);
     wrap.replaceChild(pwLabel, pwInput);
   }
-  
+
 
 
 function changeNick() {
     var changeBtn = document.createElement("input");
-    changeBtn.setAttribute("class", "btn");
+    changeBtn.setAttribute("class", "btn save nick");
     changeBtn.setAttribute("type", "button");
     changeBtn.setAttribute("value", "저장하기");
     changeBtn.setAttribute("onclick", "saveNick()");
-    
+
     var nicknameLabel = document.getElementById("nickname");
     var nicknameInput = document.createElement("input");
     nicknameInput.setAttribute("class", "inputField");
     nicknameInput.setAttribute("type", "text");
     nicknameInput.setAttribute("id", "newNickname");
-    
+
     var wrap = document.getElementsByClassName("wrap")[0];
-    
-    wrap.replaceChild(changeBtn, document.getElementsByClassName("nick")[0]);
+
+    wrap.replaceChild(changeBtn, document.getElementsByClassName("btn nick")[0]);
     wrap.replaceChild(nicknameInput, nicknameLabel);
   }
-  
+
   function saveNick() {
     var newNickname = document.getElementById("newNickname").value;
-    
-    // 여기에 닉네임 저장 또는 처리하는 코드를 추가하세요.
-    
+
+    // 여기에 닉네임 저장 코드
+
     var changeBtn = document.createElement("input");
     changeBtn.setAttribute("class", "btn nick");
     changeBtn.setAttribute("type", "button");
     changeBtn.setAttribute("value", "변경하기");
     changeBtn.setAttribute("onclick", "changeNick()");
-    
+
     var nicknameInput = document.getElementById("newNickname");
     var nicknameLabel = document.createElement("span");
     nicknameLabel.setAttribute("class", "label2");
     nicknameLabel.setAttribute("id", "nickname");
     nicknameLabel.innerText = "닉네임정보";
-    
+
     var wrap = document.getElementsByClassName("wrap")[0];
-    
-    wrap.replaceChild(changeBtn, document.getElementsByClassName("btn nick")[0]);
+
+    wrap.replaceChild(changeBtn, document.getElementsByClassName("btn save nick")[0]);
     wrap.replaceChild(nicknameLabel, nicknameInput);
   }
-  
